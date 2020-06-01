82e9c388b0c93a58a0b863974fbd69406a10f1ec
pooja <poojaanbuchezhian@gmail.com>
2020-06-01 18:11:21 +0530

    Setting React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-cloud-project"
+  },
+  apiGateway: {
+    REGION: "YOUR_API_GATEWAY_REGION",
+    URL: "https://0rljnetrvb.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_QVtRxq4jt",
+    APP_CLIENT_ID: "5bk0ln4escnbfp6dr4hiaopo7a",
+    IDENTITY_POOL_ID: "us-east-1:b233b55a-d103-4010-b9b0-49c7a79aa8a7"
+  }
+};
